17b1b9c2572ecec2f1fa9fe476be7fcf778a9a63
forme008 <estellaxabibullaeva0308@gmail.com>
2023-10-18 17:02:26 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,226 @@
+
+
+///1
+// let sonArr = []
+
+// for(let i = 1; i <=50; i++){
+//     if(i % 2 == 0){
+//         sonArr.push(i)
+//     }
+// }
+
+// console.log(sonArr);
+
+///2
+// let sonArr = []
+
+// for(let i = 1; i <=50; i++){
+//     if(i % 3 == 0){
+//         sonArr.push(i)
+//     }
+// }
+
+// console.log(sonArr);
+
+
+// 3
+// let sonArr = []
+
+// for(let i = 1; i <=400; i++){
+//     if(i % 3 == 0 && i % 5 == 0 && i % 7  == 0){
+//         sonArr.push(i)
+//     }
+// }
+
+// console.log(sonArr);
+
+
+
+// //4
+// let son = 0
+// for(let i = 1; i < 10; i++){
+// son = son + i
+// }
+
+// console.log(son);
+
+
+
+//tasks number 1
+// let i = 1;
+
+// while (i <= 50) {
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+
+//     i++
+// }
+
+//tasks number 2
+
+// let q = 1
+
+// while (q <= 50) {
+
+//     if(q > 8 && q < 10){
+//         console.log(q);
+//     }
+
+//     q++
+// }
+
+
+// let i = 1;
+
+// while(i <= 20){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+
+//     i++
+// }
+
+
+// let i = 1;
+
+// while(i <= 30){
+//     if(i % 5 != 0){
+//         console.log(i);
+//     }
+
+//     i++
+// }
+
+// let i = 1;
+
+// while(i < 33){
+//     if (i % 2 ==0) {
+//        console.log(i);
+//     }
+//     i++ 
+// }
+
+// //misol1
+// let q = 30
+// for (let i = 1; i < q ; i++){
+//     if (q % i == 0) {
+//         console.log(i);
+//     } 
+// }
+
+// //misol2
+// let w = 30
+// let e = 0
+// for (let i = 1; i < w; i++){
+//     if (w % i == 0) {
+//         e = e + i
+//     }
+// }
+
+// console.log(e);
+
+// let juftArr = []
+// for(let i = 1; i < 10; i++){
+//    if(i % 2 == 0)
+//    console.log(i);
+// }
+
+// console.log(juftArr);
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// let i = 1;
+
+// while(i <= 10){
+//     console.log();
+//     i++
+// }
+
+let fruits = prompt("mevani nomini kiriting")
+
+switch (fruits) {
+    case "banan":
+        alert("banana")
+        break;
+    case "biriba":
+        alert("Biriba")
+        break;
+    case "sapodila":
+        alert("Sapodilla")
+        break;
+    case "dag'al melotriya":
+        alert("Rough melotria")
+        break;
+    case "qora sapota":
+        alert("Black shoe")
+        break;
+    case "ilon mevasi":
+        alert("Snake fruit")
+        break;
+    case "olma":
+        alert("Apple")
+        break;
+    case "o'rik":
+        alert("Apricot")
+        break;
+    case "tarvuz":
+        alert("Watermelon")
+        break;
+    case "qovun":
+        alert("Melon")
+        break;
+    case "olxuri":
+        alert("plum")
+        break;
+    case "qulupnay":
+        alert("Strawberry")
+        break;
+    case "apelsin":
+        alert("Orange")
+        break;
+    case "kiwi":
+        alert("Kiwi")
+        break;
+    case "malina":
+        alert("Raspberry")
+        break;
+    case "uzum":
+        alert("Grapes")
+        break;
+    case "xurmo":
+        alert("Persimmon")
+        break;
+    case "kokos":
+        alert("Coconut")
+        break;
+    case "anor":
+        alert("Pomegranate")
+        break;
+    case "gilos":
+        alert("Cherry")
+        break;
+    case "shaftoli":
+        alert("Peach")
+        break;
+    case "nok":
+        alert("Pear")
+        break;
+    case "limon":
+        alert("Lemon")
+        break;
+    case "ananas":
+        alert("Pineapple")
+        break;
+    case "avocado":
+        alert("Avocado")
+        break;
+    
+
+
+    default:
+        alert("Mevani nomini to`g`ri zozing !")
+        break;
+}
